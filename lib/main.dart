11fd192efc1f028746dc94d08641d41bcdbff890
forcheng <2871979271@qq.com>
2020-05-22 09:04:57 +0800

    forcheng - add an infinite scroll ListView for WordPair

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new RandomWords()
-        ),
-      ),
+      home: new RandomWords(),
     );
   }
 }
 
 class RandomWords extends StatefulWidget {
   @override
-   createState() => new RandomWordsState();
-
+  createState() => new RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Start Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整）
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    return new ListTile(
+      title: new Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
